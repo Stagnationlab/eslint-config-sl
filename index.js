1b9316db8fe3f9a33171edce15fa426ec568ba47
Taavi Sangel <taavi@stagnationlab.com>
2016-11-03 18:37:26 +0200

    updated eslint deps, bumped version

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ module.exports = {
 	parser: 'babel-eslint',
 	rules: {
 		'indent': [2, 'tab', { SwitchCase: 1 }],
+		'no-tabs': 0,
+		'no-plusplus': 0,
+		'class-methods-use-this': 0,
 		'space-before-function-paren': [2, 'never'],
 		'no-trailing-spaces': [2, { skipBlankLines: true }],
 		'quote-props': [2, 'consistent-as-needed'],
@@ -9,6 +12,8 @@ module.exports = {
 		'react/jsx-indent': [2, 'tab'],
 		'react/jsx-indent-props': [2, 'tab'],
 		'react/jsx-no-bind': 0,
+		'react/forbid-prop-types': 0,
+		'react/no-danger': 0,
 		'react/sort-comp': [2, {
 			order: [
 				'static-methods',
@@ -22,6 +27,7 @@ module.exports = {
 		'eol-last': 'off',
 		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
 		'jsx-a11y/label-has-for': 0,
+		'jsx-a11y/no-static-element-interactions': 0,
 		'no-mixed-operators': [2, { 'allowSamePrecedence': true }],
 		'linebreak-style': [0],
 
